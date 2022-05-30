d3aa94e16c5f60a492d1877c3e66158813a780bf
anaysoul <soulyana_lakew@outlook.com>
2022-05-30 15:52:48 -0400

    randomly generating text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'typing-challenge';
+  inputText = '';
+
+  randomText = lorem.sentence();
 }
